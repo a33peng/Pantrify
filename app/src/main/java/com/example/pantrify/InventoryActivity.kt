d12d1aa6f957363fef17c9b20b72926b1f0ac51f
a33peng <ashleypeng16@gmail.com>
2025-05-28 14:24:24 -0400

    new inventory

---
+++ app/src/main/java/com/example/pantrify/InventoryActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pantrify
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class InventoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.inventory)
+
+    }
+}
