31cdb0026cc589524286a19a1e0f26e31d5b72af
a33peng <ashleypeng16@gmail.com>
2025-05-28 14:15:28 -0400

    Render new page

--- app/src/main/java/com/example/pantrify/MainActivity.kt
+++ app/src/main/java/com/example/pantrify/MainActivity.kt
@@ -110,53 +110,6 @@ fun WelcomeScreenPreview() {
     }
 }
 
-//package com.example.pantrify
-//
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import com.example.pantrify.ui.theme.PantrifyTheme
-//import androidx.compose.ui.unit.dp
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.foundation.Image
-//import androidx.compose.foundation.layout.Arrangement
-//import androidx.compose.foundation.layout.Box
-//import androidx.compose.runtime.ComposableInferredTarget
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.Row
-//import androidx.compose.ui.unit.dp
-//import androidx.compose.foundation.layout.Spacer
-//import androidx.compose.foundation.layout.height
-//import androidx.compose.foundation.layout.width
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.res.stringResource
-//import androidx.compose.foundation.background
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.layout.ContentScale
-//import androidx.compose.ui.unit.sp
-//import androidx.compose.ui.text.font.FontWeight
-//import androidx.compose.material3.Button
-//import androidx.compose.material3.ButtonDefaults
-//import androidx.compose.foundation.clickable
-//import android.content.Intent
-//
-//// responsible clicking button UI
-//import androidx.compose.runtime.getValue
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.runtime.setValue
-//import android.widget.Button
-//import android.widget.TextView
-//import androidx.compose.ui.platform.LocalContext
-//import android.widget.Toast
 
 //// keep track if start button is clicked or not
 //// var isStart by remember { mutableStateOf(false) }
--- app/src/main/java/com/example/pantrify/SecondActivity.kt
+++ app/src/main/java/com/example/pantrify/SecondActivity.kt
@@ -5,28 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import com.example.pantrify.ui.theme.PantrifyTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.compose.ui.unit.sp
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.pantrify.ui.theme.PantrifyTheme
-
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.withStyle
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.scale
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,19 +36,27 @@ class SecondActivity : ComponentActivity() {
 
 @Composable
 fun MainContent2() {
-    Box(
+    // vertical layout of mobile app
+    Column (
         modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color(0xFF75BDBC))
+            .fillMaxSize()  // fill up whole screen
+            //.background(color = Color(0xFF75BDBC)) // set background colour
+            .padding(16.dp)
     ){
+        // display annotated text style
+        Text(
+            buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color(0xFF0B0C0D), fontSize = 30.sp)) { append("Hi") } // dark colour text
+                append(" ")
+                withStyle(style = SpanStyle(color = Color(0XFF75BDBC), fontSize = 30.sp)) { append("Foodie! \n") } // light teal text
+                withStyle(style = SpanStyle(color = Color(0xFF0B0C0D))) { append("What are you craving today?")}
+                },
+            fontSize = 20.sp
+        )
 
-        //        Spacer(modifier = Modifier.height(50.dp))
-
-        Text("Search Page",
-            fontSize = 30.sp,
-            )
     }
 }
+
 
 // display preview in IDE emulator
 @Preview(showBackground = true)
@@ -61,16 +64,3 @@ fun MainContent2() {
 fun DefaultPreview2() {
     MainContent2()
 }
-
-
-//class SecondActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            PantrifyTheme {
-//                // call composable function to display elements and its contents
-//                MainContent2()
-//            }
-//        }
-//    }
-//}
